f7f3ff53b35d539b9c2bb1938b85062dfffe5dfc
Breakout Code <zachary_krewson@hotmail.com>
2017-01-28 23:02:20 -0500

    I messed up something in class Square

--- src/Square.java
+++ src/Square.java
@@ -7,7 +7,6 @@ public class Square {
     }
 
     public static void main(String args[][]) {
-
-        Crossword thePuzzle = new Crossword( new boolean[][]((true; false),(true, false[]);
+        Crossword thePuzzle = new Crossword( new boolean[][]((true, false),(true, false[]);
         System.out.println("testing r=0, c=0 :" + thePuzzle.toBeLabeled(0,0, bSquares[]);
 
