6cdf3121912114633de342c7f84c8eb9c1394951
Zachary Krewson <20171340@archwood.org>
2017-01-30 08:10:23 -0500

    Update Crossword.java

--- src/Crossword.java
+++ src/Crossword.java
@@ -1,20 +1,20 @@
 /**
- * Created by zacha on 1/24/2017.
+ * Created by Zachary on 1/24/2017.
  */
 public class Crossword extends toBeLabeled {
     private Square[][] puzzle;
 
     public Crossword(boolean[][] blackSquares) {
-        puzzle = new Square[blackSquares.length][blackSquares[0].length];
-
-        int num = 1;
-
-        for (int r = 0; r < puzzle.length; r++) {
-            for (int c = 0; c < puzzle[0].length; c++) {
+        int rows= blackSquares.length;
+        int cols= blackSquares[0].length; 
+        puzzle = new Square[rows][cols];
+        
+        int label = 1;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
                 if (toBeLabeled(r, c, blackSquares)) {
-                    puzzle[r][c] = new Square(false, num);
-                    num++;
-                } else
+                    puzzle[r][c] = new Square(false, label++);
+                } else {
                     puzzle[r][c] = new Square(blackSquares[r][c], 0);
             }
         }
